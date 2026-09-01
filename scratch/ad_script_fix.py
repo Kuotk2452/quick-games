a99a3b3c530b6cade988ff18557f8a1ba82d59b4
Kuotk2452 <Kuotk2452@users.noreply.github.com>
2026-09-01 14:51:35 -0700

    fix(ads): fix missing script tag for mock sdk in game 02

---
+++ scratch/ad_script_fix.py
@@ -0,0 +1,19 @@
+﻿import os
+import re
+
+filepath = 'games/02-wordle-survivor/index.html'
+with open(filepath, 'r', encoding='utf-8') as f:
+    c = f.read()
+
+# Add mock-reward-sdk.js before audio.js
+c = re.sub(
+    r'<script type="module" src="js/audio\.js',
+    '<script src="../../js/mock-reward-sdk.js?v=1.2"></script>\n  <script type="module" src="js/audio.js',
+    c
+)
+
+# Bump version to v=2.6
+c = re.sub(r'js/game\.js\?v=[\d\.]+', 'js/game.js?v=2.6', c)
+
+with open(filepath, 'w', encoding='utf-8') as f:
+    f.write(c)
